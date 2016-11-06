f4f673caf352c1de3603a601b263291b5b2366fb
David <david@wasylciw.com>
2016-11-06 00:34:42 -0600

    Additional functionality for FakeQuerySet and added index stats to results

--- gsa/__init__.py
+++ gsa/__init__.py
@@ -9,6 +9,7 @@ More docs here:
 
 import urllib
 import urllib2
+import itertools
 from xml.etree import ElementTree
 from django.contrib.sites.models import Site
 from gsa.queryset import FakeQuerySet
@@ -27,10 +28,10 @@ class GSA(object):
     def __init__(self, host=None):
         self.host = host
         self.data = {}
-    
+
     def query(self, q, site=None, lr=None, num=10, output="xml", client="default_frontend", **kwargs):
         """Queries GSA with given attributes.
-        
+
         Args:
             g: search phrase
             site: search in specific collection (string or list of strings)
@@ -38,10 +39,10 @@ class GSA(object):
             num: number of returned results (max 100)
             output: default output from GSA
             client: default client frontend
-            
+
             kwargs: any other query command (appended to url)
-            
-        
+
+
         Returns: GSAResult instance
         """
         self.data['q'] = q
@@ -51,31 +52,31 @@ class GSA(object):
             self.data['site'] = "|".join(site)
         if lr:
             self.data['lr'] = lr
-        
+
         self.data['num'] = str(num)
-        
-        
+
+
         self.data['output'] = output
         self.data['client'] = client
-        
+
         if not 'ie' in kwargs:
             # input encoding (query)
             self.data['ie'] = "UTF-8"
-            
+
         if not 'oe' in kwargs:
             # output encoding (result)
             self.data['oe'] = "UTF-8"
-        
+
         if kwargs:
             self.data.update(kwargs)
-        
+
         _data = ["%s=%s" % (key, urllib2.quote(unicode(value).encode('utf8'))) for key, value in self.data.items()]
         #_data = ["%s=%s" % (key, urllib2.quote(value)) for key, value in self.data.items()]
-        
+
         url = "http://%(host)s/search?%(data)s" %{
             'host': self.host,
             'data': "&".join(_data)
-        } 
+        }
         try:
             response = urllib2.urlopen(url)
         #except urllib2
@@ -83,37 +84,37 @@ class GSA(object):
             import sys
             #from traceback import format_exception
             #trace_exc = '\n'.join(format_exception(*sys.exc_info()))
-            
+
             current_site = Site.objects.get_current()
             subject = "GSA %s failed. (query: %s site: %s)" % (self.host, url, current_site)
             logger.error(subject, exc_info=sys.exc_info())
             return GSAEmptyResult()
-        
+
         return GSAResult(response)
-    
+
 
 class GSAResult(object):
     def __init__(self, response):
         self._response_data = None
         self._tree = None
         self.response = response
-    
-    
+
+
     def get_response_data(self):
         if not self._response_data:
             self._response_data = self.response.read()
             #print self._response_data
         return self._response_data
-    
-    response_data = property(get_response_data)    
-    
+
+    response_data = property(get_response_data)
+
     def get_tree(self):
         if not self._tree:
             self._tree = ElementTree.XML(self.response_data)
         return self._tree
-        
+
     tree = property(get_tree)
-    
+
     def links(self):
         for element in self.tree.getiterator("R"):
             s = element.find("S").text or ""
@@ -122,34 +123,52 @@ class GSAResult(object):
             except:
                 t = ""
             yield element.find("U").text, t, s
-    
+
     def as_queryset(self):
         queryset = FakeQuerySet(list(self.links()))
         return queryset
-          
+
+    def as_paginator_queryset(self):
+        # With null elements to be compatible with Djnago's Paginator
+        queryset = FakeQuerySet([None]*(self.start_index()-1) + list(self.links()))
+        return queryset
+
     def count(self):
         try:
             return int(self.tree.find("./RES/M").text)
         except AttributeError:
             return 0
-    
-    __unicode__ = lambda self: self.response_data    
+
+    def start_index(self):
+        # First result returned
+        try:
+            return int(self.tree.find("./RES").attrib['SN'])
+        except AttributeError:
+            return 0
+
+    def returned_index(self):
+        # Num returned / Last result returned
+        try:
+            return int(self.tree.find("./RES").attrib['EN'])
+        except AttributeError:
+            return 0
+
+    __unicode__ = lambda self: self.response_data
 
 
 class GSAEmptyResult(GSAResult):
     def __init__(self):
         super(GSAEmptyResult, self).__init__("")
-    
+
     def get_response_data(self):
         return ""
-    
-    response_data = property(get_response_data)    
-            
+
+    response_data = property(get_response_data)
+
     def links(self):
         return []
-            
+
     def count(self):
         return 0
-    
+
     __unicode__ = lambda self: self.response_data
-    
--- gsa/queryset.py
+++ gsa/queryset.py
@@ -3,14 +3,79 @@ class FakeQuerySet(object):
     
     Implements just methods required for list_detail.
     """
-    def __init__(self, items): 
+    def __init__(self, items):
+        self.model = None
         self.items = items
-        
-    def _clone(self): 
-        return self    
-    
-    def __len__(self): 
+
+    def _clone(self):
+        return self
+
+    def __len__(self):
         return len(self.items)
-    
-    def __getitem__(self, k): 
+
+    def __getitem__(self, k):
         return self.items[k]
+
+    def __iter__(self):
+        return self.items.__iter__()
+
+    def __nonzero__(self):
+        return bool(self.items)
+
+    def __repr__(self):
+        return repr(list(self))
+
+    def count(self):
+        return len(self.items)
+
+    def exists(self):
+        return bool(self.items)
+
+    def first(self):
+        if self.items:
+            return self.items[0]
+
+    def last(self):
+        if self.items:
+            return self.items[-1]
+
+    def get(self, **kwargs):
+        items = self.filter(**kwargs)
+        item_count = items.count()
+
+        if result_count == 0:
+            raise self.model.DoesNotExist("%s matching query does not exist." % self.model._meta.object_name)
+        elif result_count == 1:
+            return items[0]
+        else:
+            raise self.model.MultipleObjectsReturned(
+                "get() returned more than one %s -- it returned %s!" % (self.model._meta.object_name, result_count)
+            )
+
+    def all(self):
+        return self
+
+    def _get_filters(self, **kwargs):
+        # a list of test functions; objects must pass all tests to be included
+        # in the filtered list
+        filters = []
+
+        for key, val in kwargs.items():
+            key_clauses = key.split('__')
+            if len(key_clauses) != 1:
+                raise NotImplementedError("Complex filters with double-underscore clauses are not implemented yet")
+
+            filters.append(test_exact(self.model, key_clauses[0], val))
+
+        return filters
+
+    def filter(self, **kwargs):
+        filters = self._get_filters(**kwargs)
+
+        filtered_results = [
+            obj for obj in self.items
+            if all([test(obj) for test in filters])
+        ]
+
+        return FakeQuerySet(self.model, filtered_results)
+
